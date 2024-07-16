68c38f615842c116adb5bd4a5e7f35d817a31eec
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-07-16 22:05:21 +0700

    update vercel.json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,14 +2,18 @@ import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth/auth.service';
+import { Public } from './decorator/customize';
 
 @Controller() //  route /
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
-    private authService: AuthService
-
-  ) { }
-
+    private authService: AuthService,
+  ) {}
+  @Public()
+  @Get()
+  home() {
+    return 'home';
+  }
 }
